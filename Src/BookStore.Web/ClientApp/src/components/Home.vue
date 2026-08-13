a38d27a53060d04f6597bf2610cb32e6bf30d0a5
Felipe Faria <felipe.faria@azurebrasil.cloud>
2026-08-12 23:48:41 -0300

    Refactor button component styles and update button link appearance in Home view

--- Src/BookStore.Web/ClientApp/src/components/Home.vue
+++ Src/BookStore.Web/ClientApp/src/components/Home.vue
@@ -33,7 +33,7 @@ defineProps<{
           </p>
         </template>
         <template #footer>
-          <Button as="a" :href="booksUrl" class="w-fit">
+          <Button as="a" :href="booksUrl" class="w-fit !border-surface-700 !bg-surface-700 !text-white hover:!bg-surface-800">
             <i class="pi pi-book" />
             Open Books
           </Button>
@@ -54,7 +54,7 @@ defineProps<{
           </p>
         </template>
         <template #footer>
-          <Button as="a" :href="authorsUrl" class="w-fit">
+          <Button as="a" :href="authorsUrl" class="w-fit !border-surface-700 !bg-surface-700 !text-white hover:!bg-surface-800">
             <i class="pi pi-users" />
             Open Authors
           </Button>
